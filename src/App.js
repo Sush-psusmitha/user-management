37fb648f4f06a7d416be740833ac7e94f18cf979
Sush-psusmitha <sushsusmitha13@gmail.com>
2025-07-17 16:23:07 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {Button,EditableText} from '@blueprintjs/core';
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  
+
+  useEffect(() => {
+    async function userData() {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        if (!res.ok) {
+          throw new Error("Can't Fetch the data!");
+        }
+        const data = await res.json();
+        console.log(data);
+        setUsers(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    userData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="bp4-html-table modifier">
+        <thead>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Website</th>
+          <th>Action</th>
+        </thead>
+        <tbody>
+          {users.map(user => 
+            <tr key={user.id}>
+                 <td>{user.id}</td>
+                 <td>{user.name}</td>
+                 <td><EditableText value={user.email}/></td>
+                 <td><EditableText value={user.website}/></td>
+                 <td>Edit Delete</td>   
+                 <tr>
+                  <Button intent="primary">Update</Button>
+                  <Button intent='danger'>Delete</Button>
+                  </tr>           
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
